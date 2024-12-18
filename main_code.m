bba1feae57cc062dfc2abcf86f6f2e13c3c52fbc
msescordial <144449307+msescordial@users.noreply.github.com>
2024-12-18 23:19:49 +0800

    Add files via upload

--- main_code.m
+++ main_code.m
@@ -8,20 +8,27 @@ min_route_length = 4;
 max_route_length = 23; 
 
 % Network (Choices: mandl, india, metro_manila)
-network_name = "metro_manila";   
+network_name = "metro_manila"; 
+
 if (network_name == "metro_manila")
     incorporate_metro = 1;
+    [DistanceMatrix,TimeMatrix,TravelDemandMatrix,TerminalNodes,k_kSP,s,transfer_time, metro_line]=network_metro_manila();
+else   
+    incorporate_metro = 0;
+    metro_line = [];
+    [DistanceMatrix,TimeMatrix,TravelDemandMatrix,TerminalNodes,k_kSP,s,transfer_time]=network_mandl();   % mandl OR india
 end
 
-[DistanceMatrix,TimeMatrix,TravelDemandMatrix,TerminalNodes,k_kSP,s,transfer_time, metro_line]=network_metro_manila                                                               ();
 n = size(DistanceMatrix,1);         % n = no. of nodes
 
 % Genetic Algorithm    
 [front_pop] = NSGA_2(min_route_length, max_route_length, network_name, DistanceMatrix, TimeMatrix, TravelDemandMatrix, ...
     TerminalNodes, k_kSP, s, transfer_time, n, metro_line, incorporate_metro);
 
-% Compare to real-world routes
-s = 26;
-real_routes(TravelDemandMatrix, DistanceMatrix, TimeMatrix, transfer_time, s, n);
+if (network_name == "metro_manila")
+    % Compare to real-world routes
+    s = 26;
+    real_routes(TravelDemandMatrix, DistanceMatrix, TimeMatrix, transfer_time, s, n);
+end
 
 toc
